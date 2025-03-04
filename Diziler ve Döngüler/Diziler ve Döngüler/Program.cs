5c37bebc3e26e163e01fd65e4a6eafe91fa1edef
işyar yılmaz <isyaryilmaz2121@gmail.com>
2025-03-04 16:02:22 +0300

    Add files via upload

---
+++ Diziler ve Döngüler/Diziler ve Döngüler/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+   
+        
+        int[] sayilar = new int[5];
+        int toplam = 0;
+        Console.WriteLine("Lütfen 5 tane sayı giriniz:");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Sayı " + (i + 1) + ": ");
+            sayilar[i] = int.Parse(Console.ReadLine());
+        }
+        foreach (var sayi in sayilar)
+        {
+            toplam += sayi; 
+        }
+        double ortalama = (double)toplam / 5;
+        Console.WriteLine("Toplam: " + toplam);
+        Console.WriteLine("Ortalama: " + ortalama);
+    
+

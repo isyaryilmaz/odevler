e4cd0a06162183381b33b7ed7c10b2f6fe17d12c
işyar yılmaz <isyaryilmaz2121@gmail.com>
2025-03-04 16:06:57 +0300

    Add files via upload

---
+++ sayı tahmin oyunu/sayı tahmin oyunu/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+        Random random = new Random();
+        int bilgisayarSayisi = random.Next(1, 101); 
+        int kullaniciTahmini;
+        int tahminSayisi = 0;
+
+        Console.WriteLine("Bilgisayar 1 ile 100 arasında bir sayı tuttu. Tahmin etmeye çalışın!");
+        do
+        {
+            Console.Write("Tahmininizi girin: ");
+            kullaniciTahmini = int.Parse(Console.ReadLine());
+            tahminSayisi++;
+
+            if (kullaniciTahmini < bilgisayarSayisi)
+            {
+                Console.WriteLine("Tahmininiz küçük. Tekrar deneyin!");
+            }
+            else if (kullaniciTahmini > bilgisayarSayisi)
+            {
+                Console.WriteLine("Tahmininiz büyük. Tekrar deneyin!");
+            }
+            else
+            {
+                Console.WriteLine("Tebrikler! Doğru tahmin ettiniz.");
+                Console.WriteLine("Toplam tahmin sayınız: " + tahminSayisi);
+            }
+
+        } while (kullaniciTahmini != bilgisayarSayisi); 
+  
